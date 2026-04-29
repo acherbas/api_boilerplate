cd0871cf8f17868690737d7ba7e36f2a15fe63c8
acherbas <saber.mnasri@hotmail.com>
2026-04-29 09:04:24 +0200

    Create mailer.js

---
+++ mailer.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
+  }
+});
+
+async function sendMail(to, subject, text) {
+  await transporter.sendMail({
+    from: process.env.MAIL_FROM,
+    to,
+    subject,
+    text
+  });
+}
+
+module.exports = { sendMail };
